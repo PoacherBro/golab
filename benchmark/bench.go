8f316ff8bef2d7a363088857ccbf143e8bb3b338
Leo Liu <liuxin19900213@gmail.com>
2018-08-12 22:34:04 +0800

    initial jikeshijian project

--- benchmark/bench.go
+++ benchmark/bench.go
@@ -1,14 +1,16 @@
 package benchmark
 
 import (
+	"encoding/hex"
 	"fmt"
-	"hex"
 )
 
+// EncodeString use "fmt.Sprintf"
 func EncodeString(b []byte) string {
 	return fmt.Sprintf("%x", b)
 }
 
+// EncodeToString use "hex.EncodeToString"
 func EncodeToString(b []byte) string {
 	return hex.EncodeToString(b)
 }
