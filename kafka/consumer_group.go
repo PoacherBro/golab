02b5807fca022f9771d99b7a1e5dadd3b70f6498
Leo Liu <liuxin19900213@gmail.com>
2018-12-29 17:20:10 +0800

    consumer: implemented another group based on sarama

--- kafka/consumer_group.go
+++ kafka/consumer_group.go
@@ -3,155 +3,167 @@ package kafka
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
+	"reflect"
 	"time"
 
 	"github.com/Shopify/sarama"
 )
 
-// StatusCode represent the message status
-type StatusCode int
-
-const (
-	// StatusNormal means message is consumed firstly
-	StatusNormal StatusCode = iota
-	// StatusRetry means message is consumed more then once
-	StatusRetry
-)
-
-// Message represent the received message body from Kafka
-type Message struct {
-	// Key is used for register handler
-	Key    string `json:"key"`
-	Status int    `json:"status"`
-	Entity []byte `json:"entity"`
+// Client Kafka client
+type Client struct {
+	client sarama.Client
 }
 
-// ConsumerGroupMessageHandler will be implemented by user
-type ConsumerGroupMessageHandler interface {
-	Consume(msg *Message) error
-}
-
-type consumerGroupMsgHandler struct {
-	msgHanlder    ConsumerGroupMessageHandler
-	consumerGroup *ConsumerGroup
-	quit          chan struct{}
-	msgPool       chan *Message
-}
-
-func (h consumerGroupMsgHandler) Setup(ccs sarama.ConsumerGroupSession) error {
-	h.quit = make(chan struct{})
-	h.msgPool = make(chan *Message, 256)
-	for i := 0; i < h.consumerGroup.cfg.Workers; i++ {
-		go func(workerNo int) {
-			log.Printf("Consumer Group: worker[%d] startup", workerNo)
-			for {
-				select {
-				case msg, ok := <-h.msgPool:
-					if !ok {
-						return
-					}
-					for i := 0; i < h.consumerGroup.cfg.MaxRetry; i++ {
-						err := h.msgHanlder.Consume(msg)
-						if err == nil {
-							break
-						}
-					}
-				case <-h.quit:
-					return
-				}
-			}
-		}(i)
-	}
-	return nil
-}
-
-func (h consumerGroupMsgHandler) Cleanup(ccs sarama.ConsumerGroupSession) error {
-	h.quit <- struct{}{}
-	close(h.quit)
-	close(h.msgPool)
-	return nil
-}
-
-func (h consumerGroupMsgHandler) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
-	for msg := range claim.Messages() {
-		m := &Message{}
-		err := json.Unmarshal(msg.Value, m)
-		if err != nil {
-			log.Fatalf("Consumer Group: unexpected message struct, err[%v]", err)
-		} else {
-			h.msgPool <- m
-		}
-		sess.MarkMessage(msg, "")
-	}
-	return nil
-}
-
-// ConsumerGroup instance of high level consumer group
+// ConsumerGroup Kafka consumer groupq
 type ConsumerGroup struct {
-	clientName string
-	cfg        *ConsumerConfig
-	cg         sarama.ConsumerGroup
-	handlers   map[string]map[string]ConsumerGroupMessageHandler
+	groupID      string
+	topics       []string
+	client       *Client
+	cosumerGroup sarama.ConsumerGroup
+	groupHanlder *consumerGroupHandler
 }
 
-// NewConsumerGroup init a sarama consumer-group
-func NewConsumerGroup(cfg *ConsumerConfig) (*ConsumerGroup, error) {
-	c := sarama.NewConfig()
-	c.Version = sarama.V0_10_2_0
-	c.Metadata.RefreshFrequency = 1 * time.Minute
-	c.Consumer.Offsets.Initial = sarama.OffsetNewest
-	c.Consumer.Return.Errors = true
-	clientName := generateClientID(cfg.GroupID)
-	c.ClientID = clientName
-	cg, err := sarama.NewConsumerGroup(cfg.Brokers, cfg.GroupID, c)
+// NewClient create a new Kafka client
+func NewClient(cfg *ConsumerConfig) (*Client, error) {
+	config := sarama.NewConfig()
+	config.Metadata.RefreshFrequency = 1 * time.Minute
+	config.Consumer.Offsets.Initial = sarama.OffsetNewest
+	config.Consumer.Return.Errors = true
+	config.Version = sarama.V1_1_0_0
+	clientID := generateClientID(cfg.GroupID)
+	config.ClientID = clientID
+	client, err := sarama.NewClient(cfg.Brokers, config)
 	if err != nil {
 		return nil, err
 	}
-	go func() {
-		for err := range cg.Errors() {
-			log.Printf("Consumer Group: [%s] consumer err %v", cfg.GroupID, err)
-		}
-	}()
-
-	consumerGroup := &ConsumerGroup{
-		clientName: clientName,
-		cfg:        cfg,
-		cg:         cg,
-		handlers:   make(map[string]map[string]ConsumerGroupMessageHandler),
+	c := &Client{
+		client: client,
 	}
+	return c, nil
+}
+
+// NewConsumerGroupFromClient create consumer group from a give client
+// PLEASE NOTE: consumer group can reuse the client but NOT share the client based on Kafka specification
+func NewConsumerGroupFromClient(groupID string, topics []string, client *Client) (*ConsumerGroup, error) {
+	cg, err := sarama.NewConsumerGroupFromClient(groupID, client.client)
+	if err != nil {
+		return nil, err
+	}
+	consumerGroup := &ConsumerGroup{
+		groupID:      groupID,
+		topics:       topics,
+		client:       client,
+		cosumerGroup: cg,
+		groupHanlder: &consumerGroupHandler{
+			msgHandlers: make(map[string]*msgHandler),
+		},
+	}
+	consumerGroup.groupHanlder.parent = consumerGroup
 	return consumerGroup, nil
 }
 
-// Consume start to consume messages
-func (c *ConsumerGroup) Consume() {
-	if len(c.handlers) == 0 {
-		log.Fatalf("Consumer Group: no handlers for group [%s]", c.cfg.GroupID)
-	}
-	h := consumerGroupMsgHandler{consumerGroup: c}
-	for {
-		err := c.cg.Consume(context.Background(), c.cfg.Topic, h)
-		if err != nil {
-			log.Println(err)
-		}
-	}
-}
+// RegisterHandler rgister handler for each topic-key
+// Please NOTE: if there is a existed registered handler, will return error
+func (c *ConsumerGroup) RegisterHandler(topic, key string, encoder Encoder, handler ConsumerMessageHandler) error {
+	var err error
 
-//RegisterHandler based on topic and message key, register related hanlder
-func (c *ConsumerGroup) RegisterHandler(topic, key string, handler ConsumerGroupMessageHandler) {
-	if keyHandler, ok := c.handlers[topic]; ok {
-		if _, ok := keyHandler[key]; !ok {
-			keyHandler[key] = handler
-		} else {
-			log.Printf("Consumer Group: repeatedly registed handler for topic[%s]-key[%s]", topic, key)
+	msgHandler := &msgHandler{
+		handlers: make(map[string]ConsumerMessageHandler),
+		topic:    topic,
+		encoder:  encoder,
+	}
+	if keyHandler, ok := c.groupHanlder.msgHandlers[topic]; ok {
+		if _, exists := keyHandler.handlers[key]; exists {
+			err = fmt.Errorf("Consumer Group [%s] register same handler topic[%s]-key[%s]", c.groupID, topic, key)
+			return err
 		}
+		msgHandler = keyHandler
 	} else {
-		c.handlers[topic] = make(map[string]ConsumerGroupMessageHandler)
-		c.handlers[topic][key] = handler
+		c.groupHanlder.msgHandlers[topic] = msgHandler
+	}
+
+	defer func() {
+		if e := recover(); e != nil {
+			err = fmt.Errorf("Consumer Group [%s] register with err=%v", c.groupID, e)
+			log.Fatalln(err)
+		}
+	}()
+
+	t := reflect.TypeOf(handler)
+	paramCount := t.NumIn()
+	msgType := t.In(paramCount - 1)
+	value := reflect.New(msgType).Interface()
+	msgHandler.handlers[key] = handler
+	msgHandler.encoder = encoder
+	msgHandler.entity = value
+	return err
+}
+
+// Consume will start partition consumer for listen topics
+func (c *ConsumerGroup) Consume() {
+	for {
+		c.cosumerGroup.Consume(context.Background(), c.topics, c.groupHanlder)
 	}
 }
 
-// Close consumer group to release resources
+// Close consumer group, if its related client not closed, also close
 func (c *ConsumerGroup) Close() error {
-	return c.cg.Close()
+	if !c.client.client.Closed() {
+		c.client.client.Close()
+	}
+	return c.cosumerGroup.Close()
+}
+
+type consumerGroupHandler struct {
+	parent      *ConsumerGroup
+	msgHandlers map[string]*msgHandler
+}
+
+// ConsumerMessageHandler registered handler for each message process
+type ConsumerMessageHandler func(ctx context.Context, topic, key string, msg interface{}) error
+
+type msgHandler struct {
+	topic      string
+	entity     interface{}
+	encoder    Encoder
+	handlers   map[string]ConsumerMessageHandler // msgType -> handler
+	maxRetries int
+}
+
+func (h *consumerGroupHandler) Setup(ccs sarama.ConsumerGroupSession) error { return nil }
+
+func (h *consumerGroupHandler) Cleanup(ccs sarama.ConsumerGroupSession) error { return nil }
+
+func (h *consumerGroupHandler) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for msg := range claim.Messages() {
+		topic := claim.Topic()
+		msgHandler, ok := h.msgHandlers[topic]
+		if !ok {
+			err := fmt.Errorf("Consumer Group [%s]: no handler for topic[%s]", h.parent.groupID, topic)
+			log.Fatalln(err)
+			return err
+		}
+		consumerMsg := &Message{}
+		err := json.Unmarshal(msg.Value, consumerMsg)
+		if err != nil {
+			log.Fatalf("Consumer Group: unexpected message, err=%v\n", err)
+			return err
+		}
+		entity := msgHandler.entity
+		err = msgHandler.encoder.Decode(consumerMsg.Entity, entity)
+		if err != nil {
+			log.Fatalf("Consumer Group: handler decode value failed, err=%v\n", err)
+			continue
+		}
+		fn, ok := msgHandler.handlers[consumerMsg.Type]
+		if !ok {
+			log.Printf("Consumer Group: no handler for topic[%s]-msgType[%s]", topic, consumerMsg.Type)
+			continue
+		}
+		fn(sess.Context(), topic, consumerMsg.Type, entity)
+		sess.MarkMessage(msg, "")
+	}
+	return nil
 }
--- kafka/consumer_group_test.go
+++ kafka/consumer_group_test.go
@@ -1,6 +1,7 @@
 package kafka_test
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -16,8 +17,8 @@ type tmpConsumerGroupMsgHandler struct {
 	log *testing.T
 }
 
-func (t *tmpConsumerGroupMsgHandler) Consume(msg *kafka.Message) error {
-	logMsg := fmt.Sprintf("Receive message (key=%s, value=[%s]", string(msg.Key), string(msg.Entity))
+func (t *tmpConsumerGroupMsgHandler) Consume(ctx context.Context, topic, key string, msg interface{}) error {
+	logMsg := fmt.Sprintf("Receive message (key=%s, value=[%v]", key, msg)
 
 	t.log.Log(logMsg)
 	log.Println(logMsg)
@@ -25,41 +26,56 @@ func (t *tmpConsumerGroupMsgHandler) Consume(msg *kafka.Message) error {
 	return nil
 }
 
-func initConsumerGroup(t *testing.T) (*kafka.ConsumerGroup, error) {
+func initClient(t *testing.T) (*kafka.Client, error) {
 	cfg := &kafka.ConsumerConfig{
 		Brokers:  []string{"localhost:9092"},
 		Topic:    []string{"test1"},
 		MaxRetry: 5,
-		GroupID:  "kafka-test",
 		Workers:  3,
 	}
-	handler := &tmpConsumerGroupMsgHandler{log: t}
-	cg, err := kafka.NewConsumerGroup(cfg)
+	client, err := kafka.NewClient(cfg)
 	if err != nil {
 		return nil, err
 	}
-	cg.RegisterHandler("test1", "testkey", handler)
+	return client, nil
+}
+
+func initConsumerGroup(t *testing.T, groupID string, topic []string, client *kafka.Client) (*kafka.ConsumerGroup, error) {
+	cg, err := kafka.NewConsumerGroupFromClient(groupID, topic, client)
+	if err != nil {
+		return nil, err
+	}
 	return cg, err
 }
 
 func TestConsumerGroup(t *testing.T) {
 	log.SetFlags(log.Ldate | log.Lshortfile)
 	sarama.Logger = log.New(os.Stdout, "[sarama] ", log.LstdFlags)
-	cg, err := initConsumerGroup(t)
-	if err != nil {
-		t.Error(err)
-	}
-	defer func() {
-		if err := cg.Close(); err != nil {
-			log.Printf("Kafka Consumer Group close failed, %v", err)
-		}
-	}()
 
 	// Create signal channel
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
+	client, err := initClient(t)
+	if err != nil {
+		t.Error(err)
+	}
 
-	go cg.Consume()
+	topic := "test1"
+	handler := &tmpConsumerGroupMsgHandler{log: t}
+	for i := 0; i < 2; i++ {
+		cg, err := initConsumerGroup(t, fmt.Sprintf("kafka-test%d", i), []string{"test1"}, client)
+		if err != nil {
+			t.Error(err)
+		}
+		cg.RegisterHandler(topic, "id", kafka.JSONEncoder(), kafka.ConsumerMessageHandler(handler.Consume))
+		defer func(c *kafka.ConsumerGroup) {
+			if err := c.Close(); err != nil {
+				log.Printf("Kafka Consumer Group close failed, %v", err)
+			}
+		}(cg)
+
+		go cg.Consume()
+	}
 
 	<-sigchan
 	log.Println("Receive signal, exited")
