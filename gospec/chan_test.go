8d0eb81b47c526adbf9ae0fd596369f8ba4e80f9
Leo Liu <liuxin19900213@gmail.com>
2019-04-27 20:28:24 +0800

    测试发送pointer到chan出现panic情况

---
+++ gospec/chan_test.go
@@ -0,0 +1,60 @@
+package gospec_test
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+type User struct {
+	ID   int64
+	Name string
+}
+
+// 为了测试在关闭chan之后，仍然会出现nil pointer dereference panic
+func TestSendPointer(t *testing.T) {
+	buffer := make(chan *User, 3)
+	closeChan := make(chan bool, 1)
+
+	var wg sync.WaitGroup
+
+	// consumer
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-closeChan:
+				fmt.Println("consumer received quit")
+				return
+			case data := <-buffer:
+				time.Sleep(time.Second)
+				fmt.Println(fmt.Sprintf("%d-%s", data.ID, data.Name))
+				//			default:
+				//				fmt.Println("No data")
+			}
+		}
+	}()
+
+	// producer
+	go func() {
+		for i := int64(0); i < int64(3); i++ {
+			user := &User{
+				ID:   i,
+				Name: fmt.Sprintf("Leo%d", i),
+			}
+			buffer <- user
+			fmt.Println(fmt.Sprintf("Producer: data[%d]", i))
+		}
+		closeChan <- true
+	}()
+
+	<-closeChan
+	close(buffer)
+	fmt.Println("closed buffer")
+	wg.Wait()
+
+	close(closeChan)
+	fmt.Println("closed closeChan")
+}
