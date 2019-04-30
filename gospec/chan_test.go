9a8e2246adf3e42216d67a8fbc969fa5a4c161a2
Leo Liu <liuxin19900213@gmail.com>
2019-04-30 20:51:34 +0800

    gospce: 解释为什么close chan之后会出现nil panic

--- gospec/chan_test.go
+++ gospec/chan_test.go
@@ -13,6 +13,9 @@ type User struct {
 }
 
 // 为了测试在关闭chan之后，仍然会出现nil pointer dereference panic
+// 解释：在channel close之后如果还有数据，是可以继续读取数据的，知道最后读取空值，即nil
+// 所以可以使用两个参数接受， value, ok := <- chan，当ok为false，代表已经关闭了
+// 或者可以用 for-range的形式消费channel，这样也是在close之后退出循环
 func TestSendPointer(t *testing.T) {
 	buffer := make(chan *User, 3)
 	closeChan := make(chan bool)
@@ -29,7 +32,10 @@ func TestSendPointer(t *testing.T) {
 			case <-closeChan:
 				fmt.Println("consumer received quit")
 				return
-			case data := <-buffer:
+			case data, ok := <-buffer:
+				if !ok {
+					return
+				}
 				time.Sleep(time.Second)
 				fmt.Println(fmt.Sprintf("%d-%s", data.ID, data.Name)) // panic here
 				//			default:
