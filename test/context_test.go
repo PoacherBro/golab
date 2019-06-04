1f34571cfe817a7c4306cbc5f5aa8a056d6ae43f
Leo Liu <liuxin19900213@gmail.com>
2019-06-04 16:31:59 +0800

    gospec: context test调整代码

--- test/context_test.go
+++ test/context_test.go
@@ -11,18 +11,16 @@ import (
 // 主要是为了测试Cancel Context在父子关系中如何表现
 // 测试结果为：父context cancel后，子context也会被标记为Done；反过来则不行。
 func TestCancelContext(t *testing.T) {
-	parent := context.Background()
-	childOne, cancelChildOne := context.WithCancel(parent)
-	childTwo, cancelChileTwo := context.WithCancel(childOne)
+	parent, cancelParent := context.WithCancel(context.Background())
+	child, cancelChild := context.WithCancel(parent)
 
 	var wg sync.WaitGroup
 	finish := make(chan bool)
-	inited := make(chan bool, 3)
+	inited := make(chan bool, 2)
 
 	ctxMsg := map[context.Context]string{
-		parent:   "Parent is done",
-		childOne: "ChildOne is done",
-		childTwo: "ChildTwo is done",
+		parent: "Parent is done",
+		child:  "Child is done",
 	}
 
 	for ctx, msg := range ctxMsg {
@@ -47,7 +45,7 @@ func TestCancelContext(t *testing.T) {
 	for {
 		<-inited
 		i++
-		if i == 3 {
+		if i == 2 {
 			break
 		}
 	}
@@ -56,13 +54,18 @@ func TestCancelContext(t *testing.T) {
 	// 通过调整两个cancel的顺序，测试子context是否可以cancel父context。经测试，是不行的
 	// 但是父context可以cancel子context
 
-	fmt.Println("cancelChildOne")
-	cancelChildOne()
+	//fmt.Println("cancelParent")
+	//cancelParent()
+
+	//time.Sleep(time.Second)
+
+	fmt.Println("cancelChild")
+	cancelChild()
 
 	time.Sleep(time.Second)
 
-	fmt.Println("cancelChileTwo")
-	cancelChileTwo()
+	fmt.Println("cancelParent")
+	cancelParent()
 
 	close(finish)
 	wg.Wait()
